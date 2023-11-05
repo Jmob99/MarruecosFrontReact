cc796938f9f984dd5062dcd14c3138f6c0aeaf18
Jose M Dev <josemiguelordonez99@gmail.com>
2023-11-04 19:29:49 -0500

    se añaden rutas y modifican estilos restantes

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -6,23 +6,28 @@ import { Link } from 'react-router-dom';
 
 
 function Inicio() {
+    const linkStyles = {
+        textDecoration: 'none', // Quital el subrayado
+        color: 'inherit', // Mantiene el color de texto predeterminado
+      };
     return (
         <div>
             <Logo />
             <Titulo
                 name={"Inicio"}
             />
-            <Link to="/empleados">
+            <Link to="/empleados" style={linkStyles}>
                 <BtnOpciones
                     name={"Registro de empleados"}
+
                 />
             </Link>
-            <Link to="/">
+            <Link to="/" style={linkStyles}>
                 <BtnOpciones
                     name={"name"}
                 />
             </Link>
-            <Link to="/">
+            <Link to="/" style={linkStyles}>
                 <BtnOpciones
                     name={"name"}
                 />
