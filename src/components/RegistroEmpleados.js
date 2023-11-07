756436240eef28ddec082996a8fe8b010f9c1027
vahos030 <jonathan.vahos@misena.edu.co>
2023-11-06 22:15:50 -0500

    creacion de la ventana crear cliente

--- src/components/RegistroEmpleados.js
+++ src/components/RegistroEmpleados.js
@@ -1,95 +1,100 @@
-import React, { useState } from 'react';
-import '../styles/RegistroEmpleado.css';
-import { Titulo } from './Titulo';
-import { Logo } from './Logo';
-import { Link } from 'react-router-dom';
-
+import React, { useState } from "react";
+import "../styles/RegistroEmpleado.css";
+import { Titulo } from "./Titulo";
+import { Logo } from "./Logo";
+import { Link } from "react-router-dom";
 
 function RegistroEmpleados() {
+  const [form, setForm] = useState({});
+  const [password, setPassword] = useState(false);
 
-    const [form, setForm] = useState({});
-    const [password, setPassword] = useState(false);
+  const inputs = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const checks = (e) => {
+    const isChecked = e.target.checked;
+    setForm({
+      ...form,
+      [e.target.name]: isChecked,
+    });
 
-    const inputs = (e) => {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value,
-        })
-    }
-    const checks = (e) => {
-        const isChecked = e.target.checked;
-        setForm({
-            ...form,
-            [e.target.name]: isChecked
-
-        })
-
-        setPassword(isChecked);
-
-    }
-    return (
-        <div>
-            <Logo />
-            <Titulo
-                name={"Registro de Empleados"}
+    setPassword(isChecked);
+  };
+  return (
+    <div>
+      <Logo />
+      <Titulo name={"Registro de Empleados"} />
+      <section className="container">
+        <form className="form-registroEmpleado">
+          <div className="form-group">
+            <label htmlhtmlFor="usuario" id="label-usuario">
+              Nombre de usuario
+            </label>
+            <input
+              type="text"
+              id="usuario"
+              name="usuario"
+              className="form-control mb-3"
+              onChange={inputs}
+              value={form.usuario}
             />
-            <section className="container">
-                <form className="form-registroEmpleado">
-                    <div className="form-group">
-                        <label htmlFor="usuario" id="label-usuario">Nombre de usuario</label>
-                        <input type="text"
-                            id="usuario"
-                            name='usuario'
-                            className="form-control mb-3"
-                            onChange={inputs}
-                            value={form.usuario}
+          </div>
+          <div className="form-group">
+            <label htmlhtmlFor="email" id="label-email">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="form-control mb-3"
+              onChange={inputs}
+              value={form.email}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlhtmlFor="password" id="label-password">
+              Contraseña
+            </label>
+            <input
+              type={password ? "text" : "password"}
+              id="password"
+              className="form-control mb-3"
+              name="contraseña"
+              onChange={inputs}
+              value={form.password}
+            />
+          </div>
+          <div className="contraseña">
+            <input
+              type="checkbox"
+              id="checkbox"
+              value="Mostrar contraseña"
+              name="checkbox"
+              onChange={checks}
+            />
+            <label id="label" className="checkLabel">
+              Mostrar contraseña
+            </label>
+          </div>
+          <div className="d-flex justify-content-between">
+            <Link to="/">
+              <button type="submit" id="btnMenuPrincipal">
+                Menu Principal
+              </button>
+            </Link>
 
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="email" id="label-email">Email</label>
-                        <input type="email"
-                            id="email"
-                            name='email'
-                            className="form-control mb-3"
-                            onChange={inputs}
-                            value={form.email}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password" id="label-password">Contraseña</label>
-                        <input type={password ? 'text' : 'password'}
-                            id="password"
-                            className="form-control mb-3"
-                            name='contraseña'
-                            onChange={inputs}
-                            value={form.password}
-                        />
-                    </div>
-                    <div className="contraseña">
-                        <input type="checkbox"
-                            id="checkbox"
-                            value="Mostrar contraseña"
-                            name='checkbox'
-                            onChange={checks}
-
-
-                        />
-                        <label id="label" className='checkLabel'>Mostrar contraseña</label>
-                    </div>
-                    <div className="d-flex justify-content-between">
-                        <Link to="/">
-                            <button type="submit" id="btnMenuPrincipal">
-                                Menu Principal
-                            </button>
-                        </Link>
-
-                        <button type="submit" id="btn-registro">Registrar</button>
-                    </div>
-                </form>
-            </section>
-        </div>
-    )
+            <button type="submit" id="btn-registro">
+              Registrar
+            </button>
+          </div>
+        </form>
+      </section>
+    </div>
+  );
 }
 
-export { RegistroEmpleados }
+export { RegistroEmpleados };
